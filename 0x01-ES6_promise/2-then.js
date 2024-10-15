9510362c09e19f2209613f9a2ee8888016888e31
Yassine <hondayassin5@gmail.com>
2024-10-15 15:04:57 +0100

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
-    .catch(() => new Error());
+    });
 }
