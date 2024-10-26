5240967d52ac8d2cf0dffd03c9fa874638391ef5
Yassine <hondayassin5@gmail.com>
2024-10-26 17:48:16 +0100

    a class with getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -37,7 +28,7 @@ export default class HolbertonCourse {
   }
 
   get students() {
-    return this._length;
+    return this.students;
   }
 
   set students(values) {
