6818ee3248fc326eeacfa5995fe6faaa4bd45291
Yassine <hondayassin5@gmail.com>
2024-10-28 18:36:06 +0100

    update

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.target !== Building && this.evacuationWarningMessage !== 'function') {
+    if (this.constructor.name !== 'Building' && this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
