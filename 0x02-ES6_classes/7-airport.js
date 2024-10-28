db2e6313c3d5cf1ccf88c999efc9ea31e54d7df9
Yassine <hondayassin5@gmail.com>
2024-10-28 19:21:07 +0100

    update

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('the code should be a string');
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') throw new TypeError('the code should be a string');
+    this._code = value;
+  }
+
+  greet() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
