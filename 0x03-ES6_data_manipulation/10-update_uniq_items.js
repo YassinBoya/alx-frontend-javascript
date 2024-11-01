8becafdbc7e01aa4b75c7df3636be6895e760def
Yassine <hondayassin5@gmail.com>
2024-11-01 18:20:00 +0100

    done

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,8 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new TypeError('the map should be an instance of Map');
+    throw new Error('the map should be an instance of Map');
   }
   map.forEach((value, key) => {
     if (value === 1) map.set(key, 100);
   });
-  return new Map(map);
 }
